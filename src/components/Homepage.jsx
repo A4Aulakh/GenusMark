10425be42afbe272211a324b824b1b782ebf7e66
Sukhmanpreet <sukhmanaulakh54@gmail.com>
2023-09-27 09:23:51 +0530

    UI Updates Day 2

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import Navbar from './_Components/Navbar/Navbar'
 import ScrollNav from './_Components/ScrollingNav/ScrollNav'
@@ -5,35 +7,66 @@ import Hero from './_Components/Hero/Hero'
 import AdBox1 from './_Components/Hero/AdBox1'
 import Footer from './_Components/Footer/Footer'
 import Reg from './_Components/Login/Reg'
+import { useSelector } from 'react-redux'
+import LoginWithMob from './_Components/Login/LoginWithMob';
+import Sidebar from './_Components/Sidebar/Sidebar';
 
 const Homepage = () => {
+
+    const cssClass = useSelector((state) => state.users.cssClass);
+    const loginPage = useSelector((state) => state.users.loginPage);
+    const hamburgerEase = useSelector((state) => state.hambugerhandle.hamburgerEase);
+
     return (
         <>
-            <div className=''>
-            <div className="absolute flex justify-center items-center h-full w-full  backdrop-blur-md">
-                <Reg />
+
+
+            {loginPage && (
+                <div className={`${loginPage}`}>
+                    <span className='z-30 absolute flex justify-center items-center h-full w-full '>
+
+                        <LoginWithMob />
+
+                    </span>
+                
+                </div>)
+            }
+
+
+            {cssClass && (
+                <div className={`${cssClass}`}>
+                    <div className="z-20 absolute flex justify-center items-center h-full w-full backdrop-blur-sm">
+                        <Reg />
+                    </div>
+                    <div className="z-10 absolute inset-0 bg-black opacity-40"></div>
+                </div>
+            )}
+
+
+               {hamburgerEase && (<div className={`${hamburgerEase}`}>
+                <div className="absolute top-16 z-20 flex justify-center items-center h-full w-full backdrop-blur-sm">
+            <Sidebar/>
             </div>
-            <div className="absolute inset-0 bg-black opacity-40"></div>
-            </div>
+                </div>)}
+         
 
 
 
             <Navbar />
+
             <div className="md:px-2 2xl:px-10">
                 <ScrollNav />
 
                 <div className="md:px-20 2xl:px-30">
-
                     <Hero />
                     <AdBox1 />
                 </div>
-
-
             </div>
             <Footer />
-
         </>
-    )
+    );
+
+
 }
 
 export default Homepage
--- src/components/_Components/Navbar/Navbar.jsx
+++ src/components/_Components/Navbar/Navbar.jsx
@@ -1,80 +1,92 @@
+'use client'
+import { setHamburger } from '@/app/store/slices/hamburgerSlice';
+import { setCssClass } from '@/app/store/slices/userSlice';
 import React from 'react'
+import { useDispatch } from 'react-redux';
 
 const Navbar = () => {
 
+    const dispatch = useDispatch();
+
+    const handleHamburger = () => {
+        dispatch(setHamburger("block"));
+    }
+
+    handleHamburger()
+    
+    const handleLogin = () => {
+        dispatch(setCssClass("block"));
+    }
 
 
-const handleLoginClick =() => {
 
+    return (
+        <>
+            <nav>
+                <div className="navbar flex justify-between items-center bg-darkblue h-[60px] px-4 cursor-pointer">
+
+                    <div className="left mb-2 mr-2">
+                        <img src="./LogoWhite.png" alt="" width={170} />
+                    </div>
+
+                    {/* RIGHT SIDE OF THE NAVBAR */}
+
+                    <div className="right flex items-center gap-2">
+
+                        {/* Nation Selector */}
+
+                        <span className='hidden bg-darkbgblack gap-2 pl-3 pr-5 w-80 rounded-2xl items-center h-8 lg:flex'>
+
+                            <span className="drop-down ">
+                                <img src="./searchmain.png" alt="" width={12} />
+                            </span>
+
+                            <span className="nation text-xs">
+                                Search
+                            </span>
+
+
+                        </span>
+
+
+                        <span className='flex bg-darkbgblack gap-2 pl-3 pr-5 rounded-2xl items-center h-8'>
+
+                            <span className="drop-down ">
+                                <img className='lg:w-22' src="./dropdown.png" alt="" width={18} />
+                            </span>
+
+                            <span className="nation text-xs">
+                                India
+                            </span>
+
+
+                        </span>
+
+                        {/* Search Hamburger Night Mode and Profile */}
+
+                        <span className='flex gap-4 px-3'>
+
+                            <span className="hidden lg:block mt-[4px]">
+                                <img className='' src="./darkmode.png" alt="" width={18} />
+                            </span>
+
+                            <span id='profile' className="hidden lg:block mt-1" onClick={handleLogin} >
+                                <img className='-mt-px' src="./profile.png" alt="" width={18} />
+                            </span>
+
+
+
+                            <span  className="hamburger" onClick={handleHamburger}>
+                                <img src="./ham.png" alt="" width={24} />
+                            </span>
+                        </span>
+
+                    </div>
+
+                </div>
+            </nav>
+        </>
+    )
 }
 
-  return (
-   <>
-    <nav>
-        <div  className="navbar flex justify-between items-center bg-darkblue h-[60px] px-4">
-
-            <div className="left mb-2 mr-2">
-                <img src="./LogoWhite.png" alt="" width={170}/> 
-            </div>
-
-            {/* RIGHT SIDE OF THE NAVBAR */}
-
-            <div className="right flex items-center gap-2">
-
-                {/* Nation Selector */}
-
-                <span className='hidden bg-darkbgblack gap-2 pl-3 pr-5 w-80 rounded-2xl items-center h-8 lg:flex'>
-                    
-                <span className="drop-down ">
-                        <img  src="./searchmain.png" alt="" width={12} />
-                    </span>
-
-                    <span className="nation text-xs">
-                        Search
-                    </span>
-
-                  
-                </span>
-
-
-                <span className='flex bg-darkbgblack gap-2 pl-3 pr-5 rounded-2xl items-center h-8'>
-                    
-                <span className="drop-down ">
-                        <img className='lg:w-22' src="./dropdown.png" alt="" width={18} />
-                    </span>
-
-                    <span className="nation text-xs">
-                        India
-                    </span>
-
-                  
-                </span>
-
-                {/* Search Hamburger Night Mode and Profile */}
-
-                <span className='flex gap-4 px-3'>
-                   
-                <span className="hidden lg:block mt-[4px]">
-                        <img className='' src="./darkmode.png" alt="" width={18} />
-                    </span>
-
-                    <span id='profile' className="hidden lg:block mt-1" onClick={handleLoginClick()} >
-                        <img className='-mt-px' src="./profile.png" alt="" width={18} />
-                    </span>
-
-                   
-
-                    <span className="hamburger">
-                        <img src="./ham.png" alt="" width={24} />
-                    </span>
-                </span>
-
-            </div>
-
-         </div>
-    </nav>
-   </>
-  )
-}
-
-export default Navbar
+export default Navbar;
